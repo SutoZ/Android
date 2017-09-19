10d82911dbccd9d1802c041087a265835c1ab33f
Zolee <suto.zoli92@hotmail.com>
2017-09-19 11:49:42 +0200

    Hamburger Icon in Progress

--- DEMO/Mobile/app/src/main/java/com/example/zozo07/mobile/MainActivity.java
+++ DEMO/Mobile/app/src/main/java/com/example/zozo07/mobile/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.concretepage.android.R;
 //import com.concretepage.android.R;
 
 

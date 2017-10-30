4d87d7eda7cde9df739a03864b1ef6f0ba30975b
Zolee <suto.zoli92@hotmail.com>
2017-10-30 03:16:30 +0100

    Bar chart

--- DEMO/Mobile/app/src/main/java/com/example/zozo07/mobile/ChartActivity.java
+++ DEMO/Mobile/app/src/main/java/com/example/zozo07/mobile/ChartActivity.java
@@ -6,6 +6,11 @@ import android.support.annotation.Nullable;
 
 import com.concretepage.android.R;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 /**
  * Created by Zozo07 on 2017.10.30..
@@ -19,5 +24,26 @@ public class ChartActivity extends Activity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.id.chart_activity);
+
+        barChart = (BarChart) findViewById(R.id.bargraph);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(50f, 0));
+        barEntries.add(new BarEntry(100f, 0));
+        barEntries.add(new BarEntry(20f, 0));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Data");
+
+        ArrayList<String> theTitles = new ArrayList<>();
+        theTitles.add("Alarm");
+        theTitles.add("Sleep");
+        theTitles.add("Vacation");
+
+        BarData theData = new BarData(theTitles, barDataSet);
+        barChart.setData(theData);
+
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
+        barChart.setScaleEnabled(true);
     }
 }

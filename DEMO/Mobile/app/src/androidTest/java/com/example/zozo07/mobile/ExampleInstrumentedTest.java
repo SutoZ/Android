aaa403f8a25fbd9ab508ce135b56d54f676e7f35
Zolee <suto.zoli92@hotmail.com>
2017-10-31 03:28:40 +0100

    In progress of bar char unit tests

--- DEMO/Mobile/app/src/androidTest/java/com/example/zozo07/mobile/ExampleInstrumentedTest.java
+++ DEMO/Mobile/app/src/androidTest/java/com/example/zozo07/mobile/ExampleInstrumentedTest.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.contrib.PickerActions;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 import android.widget.TimePicker;
 
 import org.hamcrest.Matchers;
@@ -13,11 +14,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.concretepage.android.R;
+import com.github.mikephil.charting.charts.BarChart;
+
+import java.io.InterruptedIOException;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
 import static org.junit.Assert.*;
 
 /**
@@ -39,26 +48,47 @@ public class ExampleInstrumentedTest {
         //starting Alarm function.
         appContext.startActivity(intent);
     }
+
     @Test
     public void AppContext_checkPackageName_fromContext_With_GivenInputPackageName_Exception() throws Exception{
         String PACKAGE_NAME = "com.example.zozo07.mobile";
         appContext = InstrumentationRegistry.getTargetContext();
         assertEquals(PACKAGE_NAME, appContext.getPackageName());
     }
+
     @Test
     public void openAlarmActivityAppears_InterruptedException() throws InterruptedException {
         onView(withId(R.id.alarmToggle)).perform(click());
         Thread.sleep(1000);
     }
-//asadasad
+
     @Test
     public void setTimeOnTimePicker_InterruptedException() throws InterruptedException{
         int hour = 10;
         int minute = 55;
 
         onView(withId(R.id.alarmTimePicker)).perform(click());
-        Object name = onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
+        //Object name = onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
         onView(withClassName(Matchers.equalTo(TimePicker.class.getName()))).perform(PickerActions.setTime(hour,minute));
         Thread.sleep(2000);
     }
+
+    @Test
+    public void checkIfBarChartIsDisplayed_InterruptedException() throws InterruptedException {
+        appContext = InstrumentationRegistry.getTargetContext();
+        Intent intent = new Intent(appContext, ChartActivity.class);
+        appContext.startActivity(intent);
+        onView((withId(R.id.bargraph))).check(matches(isDisplayed()));
+        Thread.sleep(500);
+    }
+
+    @Test
+    public void checkIfBarChartX_AxisIsDisplayed_InterruptedException() throws InterruptedException {
+        appContext = InstrumentationRegistry.getTargetContext();
+        Intent intent = new Intent(appContext, ChartActivity.class);
+        appContext.startActivity(intent);
+        onView((withId(R.id.bargraph))).check(matches(withText("Alarm")));
+        Thread.sleep(500);
+    }
+
 }
